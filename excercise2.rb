6548a9d9f38fa2a40dbcbb7d218ee6876446efd7
austin shymanskigit config user.email austin.shymanski@gmail.com <Work@Austins-MacBook-Air.local>
2013-10-22 12:32:38 -0400

    excercise2

---
+++ excercise2.rb
@@ -0,0 +1,11 @@
+puts "please enter the bill amount"
+money = gets.chomp
+money = money.to_i
+
+tip = money * 0.15
+
+puts "you should leave a #{tip}$ tip."
+
+multi = 45628 * 7839
+puts "We multiplied 45628 with 7839 and got #{multi}"
+
